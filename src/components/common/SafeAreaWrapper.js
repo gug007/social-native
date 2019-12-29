0f2e1cde3ebd33a96a77d04321481d3532c3ceb6
Gug007 <gurgen.abagyan@gmail.com>
2019-12-29 04:21:07 +0200

    update safe area

--- src/components/common/SafeAreaWrapper.js
+++ src/components/common/SafeAreaWrapper.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import {
-  TouchableWithoutFeedback,
-  KeyboardAvoidingView,
-  SafeAreaView,
-  StyleSheet,
-  Keyboard,
-} from 'react-native';
+import {KeyboardAvoidingView, SafeAreaView, StyleSheet} from 'react-native';
 import useBarHeight from '../../hooks/useBarHeight';
 
 const SafeAreaWrapper = ({children}) => {
   const barHeight = useBarHeight();
 
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <SafeAreaView style={styles.container}>
-        <KeyboardAvoidingView
-          keyboardVerticalOffset={barHeight}
-          style={styles.container}
-          behavior="padding">
-          {children}
-        </KeyboardAvoidingView>
-      </SafeAreaView>
-    </TouchableWithoutFeedback>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={barHeight}
+        style={styles.container}
+        behavior="padding">
+        {children}
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
